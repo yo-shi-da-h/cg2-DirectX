00283abd084650d61ae254ed22731b3fcb8ca3d5
LC1D_ヨシダ_ハルト <146914696+yo-shi-da-h@users.noreply.github.com>
2024-10-28 14:28:57 +0900

    a

--- Input.h
+++ Input.h
@@ -3,7 +3,6 @@
 #include "wrl.h"
 #define DIRECTINPUT_VERSION 0x0800
 #include <dinput.h>
-using namespace Microsoft::WRL;
 class Input
 {
 public:
--- main.cpp
+++ main.cpp
@@ -2087,6 +2087,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	//入力解放
 	delete input;
+	
 
 	ImGui_ImplDX12_Shutdown();
 	ImGui_ImplWin32_Shutdown();
